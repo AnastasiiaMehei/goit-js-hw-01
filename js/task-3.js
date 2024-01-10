e5b2cd19c3f5dd5a9371290d93dceb217f7a0f8e
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-01-10 19:00:38 +0200

    3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth (content, padding, border) {
-return (`${content} + ${padding} + ${padding} + ${border} + ${border} + "px"`);
+return (`${content} + ${padding} + ${padding} + ${border} + ${border}`);
 };
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
